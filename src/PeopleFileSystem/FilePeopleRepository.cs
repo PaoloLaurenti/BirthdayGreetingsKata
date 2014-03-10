59631d0701ea3cfd28f31cc93c59b71e1d15776a
Paolo Laurenti <laurentipaolo@gmail.com>
2014-03-10 17:55:17 +0100

    FilePeopleRepository provides any people when file is empty

--- src/PeopleFileSystem/FilePeopleRepository.cs
+++ src/PeopleFileSystem/FilePeopleRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using BirthdayGreetings;
 
 namespace PeopleFileSystem
@@ -15,8 +16,27 @@ namespace PeopleFileSystem
 
         public IEnumerable<PersonDto> GetAll()
         {
-            File.ReadLines(_filePath);
-            return null;
+            ReadLines();
+
+            return new List<PersonDto>();
+        }
+
+        private IEnumerable<string> ReadLines()
+        {
+            var lines = new List<string>();
+
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException();
+
+            using (var fs = new FileStream(_filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 0x1000, FileOptions.SequentialScan))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    lines.Add(line);
+            }
+
+            return lines;
         }
     }
 }
--- src/Tests/PeopleFileSystemTests/FilePeopleRepositoryTest.cs
+++ src/Tests/PeopleFileSystemTests/FilePeopleRepositoryTest.cs
@@ -9,15 +9,44 @@ namespace PeopleFileSystemTests
     public class FilePeopleRepositoryTest
     {
         private string _notExistingFilePath;
+        private static string _emptyFilePath;
 
         [Test]
-        public void ShouldThrowExceptionWhenFileDoesNotExist()
+        public void ShouldThrowExceptionProvidingAllPeopleWhenFileDoesNotExist()
         {
             _notExistingFilePath = @"C:\NotExistingPath";
 
             var sut = new FilePeopleRepository(_notExistingFilePath);
 
             Executing.This(() => sut.GetAll()).Should().Throw<FileNotFoundException>();
+        }
+
+        [Test]
+        public void ShouldProvideAnyPeopleWhenFileIsEmpty()
+        {
+            CreateEmptyFile();
+
+            var sut = new FilePeopleRepository(_emptyFilePath);
+
+            var people = sut.GetAll();
+
+            people.Should().Be.Empty();
+
+            File.Delete(_emptyFilePath);
+        }
+
+        private static void CreateEmptyFile()
+        {
+            _emptyFilePath = GetFilePath();
+
+            var emptyFile = File.Create(_emptyFilePath);
+            emptyFile.Close();
+        }
+
+        private static string GetFilePath()
+        {
+            var tempFolderPath = Path.GetTempPath();
+            return Path.Combine(tempFolderPath, Path.GetTempFileName());
         }
     }
 }
