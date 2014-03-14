3329aec1c30742ecd2b9805351d9d961ba5673cd
Paolo Laurenti <laurentipaolo@gmail.com>
2014-03-14 17:24:11 +0100

    Greetings Delivery Service sends no greetings when there aren't greetings to be sent

--- src/BirthdayGreetings/GreetingDto.cs
+++ src/BirthdayGreetings/GreetingDto.cs
@@ -2,7 +2,6 @@
 {
     public class GreetingDto
     {
-        //public string Address { get; set; }
         public string Email { get; set; }
         public string Text { get; set; }
     }
---
+++ src/Tests/MailGreetingsServiceTests/GreetingsDeliveryServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BirthdayGreetings;
+using MailGreetingsService;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace MailGreetingsServiceTests
+{
+    [TestFixture]
+    public class GreetingsDeliveryServiceTest
+    {
+        private readonly string _mailFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Tmp");
+
+        [SetUp]
+        public void Init()
+        {
+            if (Directory.Exists(_mailFolderPath))
+                Directory.Delete(_mailFolderPath, true);
+
+            Directory.CreateDirectory(_mailFolderPath);
+        }
+
+        [Test]
+        public void ShuldSendNoGreetingsWhenThereAreNoGreetingsToSend()
+        {
+            var sut = new MailGreetingsDeliveryService();
+
+            sut.Deliver(new List<GreetingDto>());
+
+            GetMailSent().Count().Should().Be.EqualTo(0);
+        }
+
+        private IEnumerable<FileStream> GetMailSent()
+        {
+            return Directory.GetFiles(_mailFolderPath).Select(File.OpenRead).ToList();
+        }
+    }
+}
