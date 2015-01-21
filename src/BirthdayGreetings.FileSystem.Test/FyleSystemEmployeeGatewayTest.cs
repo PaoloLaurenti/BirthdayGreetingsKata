59bf55b3cb199f6288de22055363b2cf92bf66c1
Paolo Laurenti <laurentipaolo@gmail.com>
2015-01-21 16:02:05 +0100

    FileSystemEmployeeGateway provide one employee when file contains only one employee information

--- src/BirthdayGreetings.FileSystem.Test/FyleSystemEmployeeGatewayTest.cs
+++ src/BirthdayGreetings.FileSystem.Test/FyleSystemEmployeeGatewayTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BirthdayGreetings.Core.Employees;
 using FluentAssertions;
 using Xunit;
 
@@ -6,18 +10,50 @@ namespace BirthdayGreetings.FileSystem.Test
 {
     public class FyleSystemEmployeeGatewayTest
     {
+        private string _employeeFileFullPath;
+        private readonly FileSystemEmployeeGateway _sut;
+
+        public FyleSystemEmployeeGatewayTest()
+        {
+            PrepareEmployeeFile();
+            _sut = new FileSystemEmployeeGateway(_employeeFileFullPath);
+        }
+
         [Fact]
         public void Should_provide_an_empty_list_of_employees_when_file_is_empty()
         {
-            var sut = new FileSystemEmployeeGateway();
-
-            var employees = sut.GetEmployees().ToList();
+            File.WriteAllText(_employeeFileFullPath, "");
+            
+            var employees = _sut.GetEmployees().ToList();
 
             employees.Any().Should().BeFalse("It should not provide anything when file is empty");
         }
 
+        [Fact]
+        public void Should_provide_a_list_with_one_employee_when_file_contains_only_one_employee_information()
+        {
+            var employeeOnFile = new EmployeeDto("EmployeeLastName", "EmployeeFirstName", DateTime.Now.AddYears(-30).Date, "EmployeeEmail");
+            PutEmployeesInformationInEmployeesFile(new List<EmployeeDto> { employeeOnFile });
+
+            var employees = _sut.GetEmployees().ToList(); 
+
+            employees.Single().ShouldBeEquivalentTo(employeeOnFile, "It shoud provide the only employee listed in the employee file");
+        }
+
+        private void PrepareEmployeeFile()
+        {
+            _employeeFileFullPath = Path.GetTempFileName();
+            File.WriteAllLines(_employeeFileFullPath, new List<string> {"last_name, first_name, date_of_birth, email"});
+        }
+
+        private void PutEmployeesInformationInEmployeesFile(IEnumerable<EmployeeDto> employeesToPutInFile)
+        {
+            var employeesRows = employeesToPutInFile.Select(x => string.Format("{0}, {1}, {2}, {3}", x.LastName, x.FirstName, x.DateOfBirth.ToString("yyyy/MM/dd"), x.Email));
+            File.AppendAllLines(_employeeFileFullPath, employeesRows);
+        }
+
         //TODO LIST
-        // Should_provide_a_list_with_one_employee_when_file_contains_only_one_employee_information()
+        // Should_provide_an_empty_list_when_employees_file_contains_only_the_header_row
         // Should_provide_a_list_with_all_employees_present_in_the_file
         // Should_raise_exception_if_it_is_unable_to_interpret_employees_information
         // Should_raise_exception_when_it_is_unable_to_access_file
--- src/BirthdayGreetings.FileSystem/FileSystemEmployeeGateway.cs
+++ src/BirthdayGreetings.FileSystem/FileSystemEmployeeGateway.cs
@@ -1,13 +1,42 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using BirthdayGreetings.Core.Employees;
 
 namespace BirthdayGreetings.FileSystem
 {
     public class FileSystemEmployeeGateway : IEmployeeGateway
     {
+        private readonly string _employeeFileFullPath;
+
+        public FileSystemEmployeeGateway(string employeeFileFullPath)
+        {
+            _employeeFileFullPath = employeeFileFullPath;
+        }
+
         public IEnumerable<EmployeeDto> GetEmployees()
         {
-            return new List<EmployeeDto>();
+            var employeesOnFile = new List<EmployeeDto>();
+            var employeesRows = File.ReadAllLines(_employeeFileFullPath).Skip(1).ToList();
+            if (employeesRows.Any())
+            {
+                var employeeRow = employeesRows.First();
+                employeesOnFile.Add(CreateEmployeeDtoBy(employeeRow));
+            }
+            return employeesOnFile;
+        }
+
+        private static EmployeeDto CreateEmployeeDtoBy(string employeeRow)
+        {
+            var employeeData = employeeRow.Split(',').Select(x => x.Trim()).ToList();
+            return new EmployeeDto(employeeData[0], employeeData[1], GetDateOfBirth(employeeData), employeeData[3]);
+        }
+
+        private static DateTime GetDateOfBirth(IReadOnlyList<string> employeeData)
+        {
+            return DateTime.ParseExact(employeeData[2], "yyyy/MM/dd", CultureInfo.InvariantCulture);
         }
     }
 }
