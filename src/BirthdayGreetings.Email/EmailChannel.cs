e2db4232c47029ca90d81c0b06e8eeece6bbf097
Paolo Laurenti <laurentipaolo@gmail.com>
2015-01-28 13:44:54 +0100

    Add Email channel communication management in order to send greetings

---
+++ src/BirthdayGreetings.Email/EmailChannel.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mail;
+
+namespace BirthdayGreetings.Email
+{
+    public class EmailChannel : IEmailChannel
+    {
+        private readonly string _host;
+        private readonly int _port;
+
+        public EmailChannel(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Send(MailMessage email)
+        {
+            using (var client = new SmtpClient(_host, _port))
+            {
+                client.Send(email); 
+            }
+        }
+    }
+}
